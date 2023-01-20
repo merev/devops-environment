7598988cc338e6f751e3b37c1edeaf1b458bf614
merev <101055426+merev@users.noreply.github.com>
2023-01-21 01:21:27 +0200

    Update start-kafka.sh

--- shared/terraform/start-kafka.sh
+++ shared/terraform/start-kafka.sh
@@ -4,9 +4,3 @@
 echo "* Run the kafka cluster and the consumer-producer app ..."
 terraform -chdir=kafka-cluster init
 terraform -chdir=kafka-cluster apply -auto-approve
-
-echo "* Wait till the cluster is ready ..."
-sleep 15
-
-echo "* Run the kafka-exporter ..."
-docker container run -d --rm -p 9308:9308 danielqsj/kafka-exporter --kafka.server=192.168.99.103:9092
